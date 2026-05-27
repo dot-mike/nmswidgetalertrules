cf16ee0e1e61b1323d4929ccadd7b569ad75ad11
dot-mike <586280+dot-mike@users.noreply.github.com>
2026-05-27 16:00:26 +0200

    fix: add typing. fixes #1

--- src/Http/Controllers/Select/AlertRulesController.php
+++ src/Http/Controllers/Select/AlertRulesController.php
@@ -5,22 +5,24 @@ namespace DotMike\NmsWidgetAlertRules\Http\Controllers\Select;
 use App\Models\AlertRule;
 use App\Http\Controllers\Select\SelectController;
 
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Http\Request;
+
 class AlertRulesController extends SelectController
 {
-    protected function searchFields($request)
+    protected function searchFields(Request $request): array
     {
         return ['name'];
     }
 
-    protected function baseQuery($request)
+    protected function baseQuery(Request $request): EloquentBuilder|Builder
     {
         return AlertRule::select('id', 'name');
     }
 
-    /**
-     * @param  AlertRule  $alert_rule
-     */
-    public function formatItem($alert_rule)
+    public function formatItem(Model $alert_rule): array
     {
         return [
             'id' => $alert_rule->id,
